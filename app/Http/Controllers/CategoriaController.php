42cb687a2d92a5cd40eaf1b89d0df047cc871b94
Gabriel <josegabrielpereira2017@gmail.com>
2024-02-13 09:09:13 -0300

    Tela cadastro de categorias (versão inicial).

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CategoriaController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Create a new categoria instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\Models\Categoria
+     */
+    protected function create()
+    {
+        return view('categoria.cadastro');
+    }
+
+    /**
+     * Create a new categoria instance after a valid registration.
+     *
+     * @param   Request $request
+     * @return \App\Models\Categoria
+     */
+    protected function store(Request $request)
+    {
+        $data = $request->all();
+        $validator =  Validator::make($data, [
+            'nome' => ['required', 'string', 'max:255', 'unique:categorias'],
+            'descricao' => ['required', 'string', 'max:255'],
+        ]);
+
+        if($validator->fails()) {
+            return redirect('/cadastroCategoria')->withErrors($validator)->withInput();
+        }
+
+        Categoria::create([
+            'nome' => $data['nome'],
+            'descricao' => $data['descricao'],
+        ]);
+
+        return redirect('/cadastroCategoria');
+    }
+}
---
+++ resources/views/categoria/cadastro.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Cadastrar Categoria') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('cadastroCategoria') }}">
+                        @csrf
+
+                        <div class="row mb-3">
+                            <label for="nome" class="col-md-4 col-form-label text-md-end">{{ __('Nome') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="nome" type="text" class="form-control @error('nome') is-invalid @enderror" name="nome" value="{{ old('nome') }}" required autocomplete="nome" autofocus>
+
+                                @error('nome')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="descricao" class="col-md-4 col-form-label text-md-end">{{ __('descricao') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="descricao" type="text" class="form-control @error('descricao') is-invalid @enderror" name="descricao" value="{{ old('descricao') }}" required autocomplete="descricao" autofocus>
+
+                                @error('descricao')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Cadastrar') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
